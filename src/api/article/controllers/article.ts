9a75b6457d99a6dff3a8b9b5ac9963ee8a34b3f9
KristinaGordey <144432505+KristinaGordey@users.noreply.github.com>
2025-09-28 13:36:13 +0300

    fix: 1 Исправление кастомного контроллера, добавление автоинкремента просмотров

--- src/api/article/controllers/article.ts
+++ src/api/article/controllers/article.ts
@@ -2,6 +2,24 @@
  * article controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::article.article');
+export default factories.createCoreController('api::article.article', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.entityService.findOne('api::article.article', id, {
+      populate: ['coverImage', 'category', 'user'], 
+    });
+
+	await strapi.entityService.update('api::article.article', id, {
+      data: {
+        views: (entity.views || 0) + 1,
+      },
+    });
+
+	entity.views = (entity.views || 0) + 1;
+
+    return this.transformResponse(entity);
+  },
+}));
