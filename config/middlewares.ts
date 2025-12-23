562255f209b82cbda1e32e98bcea7db78bd3a415
AliaksandraVasilevich <aleksandravasilevic9@gmail.com>
2025-12-23 17:30:56 +0300

    feat: 2 Добавлено подключение к базе

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,49 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      origin: ['http://localhost:5174'], // разрешаем фронт
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
-      credentials: true,
+import { config } from "process";
+
+export default ({ env }) => [
+    "strapi::logger",
+    "strapi::errors",
+    "strapi::security",
+    // {
+    //     name: "strapi::security",
+    //     config: {
+    //         contentSecurityPolicy: {
+    //             useDefaults: true,
+    //             directives: {
+    //                 "connect-src": ["'self'", "https:"],
+
+    //                 "img-src": [
+    //                     "'self'",
+    //                     "data:",
+    //                     "blob:",
+
+    //                     "https://euksjufaiulmowpryqiq.supabase.co",
+    //                 ],
+    //                 "media-src": [
+    //                     "'self'",
+    //                     "data:",
+    //                     "blob:",
+
+    //                     "https://euksjufaiulmowpryqiq.supabase.co",
+    //                 ],
+    //                 upgradeInsecureRequests: null,
+    //             },
+    //         },
+    //     },
+    // },
+    {
+        name: "strapi::cors",
+        config: {
+            origin: ["http://localhost:5174"], // разрешаем фронт
+            methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            headers: ["Content-Type", "Authorization"],
+            credentials: true,
+        },
     },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+    "strapi::poweredBy",
+    "strapi::query",
+    "strapi::body",
+    "strapi::session",
+    "strapi::favicon",
+    "strapi::public",
 ];
